cd319e017627ba0a246c3ad972f79a814aa88a4a
raghadalzghoul <alzghoulraghad15@gmail.com>
2026-04-11 23:02:14 +0300

    first commit

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -6,9 +6,13 @@ const sendEmail = async (to, subject, text) => {
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
-    }
+    },
+    connectionTimeout: 10000, // 10 seconds
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
   });
 
+  await transporter.verify(); // ← add this, it will throw immediately if credentials are wrong
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
     to,
